6398d54e7f460a5a0fb34c27594bb04ae6d8aa3a
Niteshkumar10452 <77620163+Niteshkumar10452@users.noreply.github.com>
2021-04-11 19:11:32 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ocr.github.io
+# html
+ 
---
+++ index.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var img = document.getElementById("scream");
+   ctx.drawImage(img);
+};
