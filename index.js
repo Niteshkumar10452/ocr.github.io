abb140ca38ab2107ffae8521116790f2a8db9792
Niteshkumar10452 <77620163+Niteshkumar10452@users.noreply.github.com>
2021-04-14 04:32:49 -0700

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-    var img = document.getElementById("scream");
-   ctx.drawImage(img);
-};
